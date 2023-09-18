b771a103a2f42540a282cdee9b4821fcf669e4c3
EdoCosic <145214281+EdoCosic@users.noreply.github.com>
2023-09-18 20:14:04 +0200

    Update and rename pronadiM.cpp to PR1-PronadiM.cpp

---
+++ PR1-PronadiM.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int PronadjiM(int n) {
+	int minM = INT_MAX;
+	for (int k = 2; k < n; k++)
+	{
+		for (int l = 2; l < n; l++)
+		{
+			int m = pow(k, l);
+			if (m >= n && m <= minM)
+				minM = m;
+		}
+	}
+	return minM;
+}
+
+int main()
+{
+	int n;
+	do {
+		cout << "Unesite prirodni broj" << endl;
+		cin >> n;
+	} while (n < 1);
+	cout << "To je broj: " << PronadjiM(n);
+	system("pause>0");
+	return 0;
+}
